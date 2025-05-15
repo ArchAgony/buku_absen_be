18c96d93f89fadd11a84e839089471537e3550c2
Daoa Ari <davaariardiansyah01@gmail.com>
2025-05-15 07:42:44 +0700

    tamu

---
+++ database/migrations/2025_05_15_001921_create_tamus_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tamus', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_tamu');
+            $table->date('tanggal_datang');
+            $table->string('alamat');
+            $table->integer('no_hp');
+            $table->string('ttd');
+            $table->unsignedBigInteger('keperluan_id');
+            $table->foreign('keperluan_id')->references('id')->on('keperluans')->onUpdate("cascade")->onDelete("cascade");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tamus');
+    }
+};
